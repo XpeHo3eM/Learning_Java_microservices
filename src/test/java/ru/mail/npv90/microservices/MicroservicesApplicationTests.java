15081b5b75b1b1bb05a8f1569db94ece3af8e784
Никулин Павел <npv90@mail.ru>
2025-05-29 12:06:23 +0400

    Init

---
+++ src/test/java/ru/mail/npv90/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mail.npv90.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
